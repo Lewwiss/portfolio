62c4d5f9e947d2823237900479b946f59061f716
Lewis <46723732+Lewwiss@users.noreply.github.com>
2022-04-24 19:14:44 +0100

    Stuff

--- components/Landing.js
+++ components/Landing.js
@@ -17,7 +17,7 @@ const Landing = () => {
                     </a>
                     <a className="text-sm text-slate-400 font-medium flex flex-row items-center rounded-sm" target="_blank" href="mailto:lewiswinthrop27@gmail.com">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3.5" viewBox="0 0 20 20" fill="#94a3b8">
-                            <path fillRule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z" clip-rule="evenodd"></path>
+                            <path fillRule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z" clipRule="evenodd"></path>
                         </svg>
                         lewiswinthrop27@gmail.com
                     </a>
