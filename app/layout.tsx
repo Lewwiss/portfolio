ed97ecb46ca6b8e9d7260c101937ff39bb29b23a
Lewis <46723732+Lewwiss@users.noreply.github.com>
2024-12-08 21:29:21 +0000

    adding html background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${config.mode === 'dark' ? 'bg-background-950': 'bg-white'}`}>
+      <script async src="http://localhost:3001/feedback-widget.js"></script>
       <body className={`${config.mode === 'dark' ? 'dark': ''}`}>
         {children}
       </body>
