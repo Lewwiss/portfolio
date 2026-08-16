7da8ee8f0ae5995a4602a3f29a17a69675b9cd4a
Lewis <46723732+Lewwiss@users.noreply.github.com>
2026-08-16 20:43:38 +0100

    upgraded next

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     validateRSCRequestHeaders: true,
+  },
+  turbopack: {
+    root: path.join(__dirname),
   },
 };
 
